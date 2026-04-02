7302dc01fc47bf417a8bc2386a1a2c2e628f6740
Wal1yS <valera.tinyakov@gmail.com>
2026-04-02 19:50:29 +0300

    Project Initialization

---
+++ backend/src/main/java/com/example/baam2/Baam2Application.java
@@ -0,0 +1,13 @@
+package com.example.baam2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baam2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baam2Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/baam2/Baam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baam2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baam2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
