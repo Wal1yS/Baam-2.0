21e1ec8f1216c268c2815c1d3d6f1c4d84a1fa7f
Wal1yS <valera.tinyakov@gmail.com>
2026-04-09 23:39:45 +0300

    Lombok DTO -> records DTO CASCADE deletion of attendance added at session or user deletion On session's owner deletion null replacement added

--- backend/src/main/java/com/example/baam2/dto/request/SessionCreateDTO.java
+++ backend/src/main/java/com/example/baam2/dto/request/SessionCreateDTO.java
@@ -1,12 +1,6 @@
 package com.example.baam2.dto.request;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class SessionCreateDTO {
-    private String title;
-    private Long ownerId;
-}
+public record SessionCreateDTO (
+    String title,
+    Long ownerId
+){}
--- backend/src/main/java/com/example/baam2/repository/AttendanceRepository.java
+++ backend/src/main/java/com/example/baam2/repository/AttendanceRepository.java
@@ -2,6 +2,8 @@ package com.example.baam2.repository;
 import com.example.baam2.model.SessionModel;
 import com.example.baam2.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.example.baam2.model.AttendanceModel;
 
@@ -11,4 +13,12 @@ import java.util.List;
 public interface AttendanceRepository extends JpaRepository<AttendanceModel, Long> {
     boolean existsByUserIdAndSessionId(Long userId, Long sessionId);
     List<AttendanceModel> findAllByUserId(Long id);
+
+    @Modifying
+    @Query("DELETE FROM AttendanceModel a WHERE a.user.id = :id")
+    void deleteByUserId(Long id);
+
+    @Modifying
+    @Query("DELETE FROM AttendanceModel a WHERE a.session.id = :id")
+    void deleteBySessionId(Long id);
 }
